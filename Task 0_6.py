8f71ad805fdcbfc7d321b15830901115abb6b496
philaSimelane <86529871+philaSimelane@users.noreply.github.com>
2021-07-14 08:37:14 +0200

    Add files via upload

---
+++ Task 0_6.py
@@ -0,0 +1,11 @@
+def maximum(num1, num2, num3):
+    if num1 > num2 and num1 > num3:
+        return num1
+    elif num2 > num1 and num2 > num3:
+        return num2
+    else:
+        return num3
+
+
+max_value = maximum(3, 5, 4)
+print(f"Maximum value: {max_value}")
